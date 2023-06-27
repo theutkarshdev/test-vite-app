80d667640ee9dacdcc8599d25d7aed440ad6db02
utkarsh-radiant <utkarsh.uitest@gmail.com>
2023-06-27 10:57:34 +0530

    camera for ios

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import "./style.scss";
 
 import TopBanner from "../../components/topbanner/TopBanner";
-import Camera from "../../components/Camera";
 
 const Home = () => {
   return (
     <div className="home_page">
       <TopBanner pgName={"Home"} pgDesc={"This is home page description"}/>
-      <Camera/>
     </div>
   );
 };
